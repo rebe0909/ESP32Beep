1d53dfc2ebb416b805a9c7558f20eca79c5283cf
Gitea <gitea@fake.local>
2022-02-18 23:26:23 +0100

    Fist Version

---
+++ beep.h
@@ -0,0 +1,15 @@
+#include <Arduino.h>
+#include <mSecTimer.h>
+
+
+class Beep {
+
+    public:
+        Beep(uint8_t beepChannel, double freq, uint8_t resolution, uint8_t BeepPin);
+        void StartBeep1(unsigned int len);
+        void StartBeep2(unsigned int len, unsigned int pause);
+        void StartBeep3(unsigned int len, unsigned int pause, unsigned int count);
+        void CheckAllBeeps(void);
+    private:
+    
+};
